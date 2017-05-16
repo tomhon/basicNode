5c8d91c0d7a72eb7d23827866aa9e44e4c31ad46
Tom Honeybone <tomhon@microsoft.com>
2017-05-16 12:04:39 -0700

    added info to console.log

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = restify.createServer();
 
 server.listen(process.env.port || 3978, function () {
 
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', 'makeable' , 'url'); 
 
 });
 
