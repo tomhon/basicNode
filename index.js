bedd1f762f08173fcafd3444668816540167485b
Tom Honeybone <tomhon@microsoft.com>
2017-05-16 12:22:27 -0700

    set to port 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 
 var server = restify.createServer(); 
 
-server.listen(process.env.port || 3978, function () {
+server.listen(80, function () {
 
     console.log('%s listening to %s', 'makeable' , 'url'); 
 
